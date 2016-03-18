51d2b4b347f21279c36f959076d003341f71bf66
kingkanbai <tassanee.oc@gmail.com>
2016-03-18 14:46:34 +0000

    rename

--- ruby/lab6.rb
+++
@@ -1,8 +0,0 @@
-# 6.1
-puts "Print all the names and bank balances"
-hash = {:name =>"Polawat" ,:bank_balances => "999999999"}
-hash.each(){|key,value| puts "#{key}:#{value}"}
-puts
-
-# 6.2
-puts "Answer : "
