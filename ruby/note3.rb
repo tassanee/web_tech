eb511ac25c1d5bee0cc483e888f1c6cd22561bc3
kingkanbai <tassanee.oc@gmail.com>
2016-03-18 14:23:56 +0000

    first commit

---
+++ ruby/note3.rb
@@ -0,0 +1,28 @@
+# 3.1
+arr = %w(a b c d e f)
+puts arr[1..3]
+puts
+
+# 3.2
+puts arr[0,2]
+puts
+
+# 3.3
+puts arr[-1]
+puts
+
+# 3.4
+arr.delete('c')
+puts arr
+puts 
+
+# 3.5
+puts "Generate a new string which is the first 20 characters of the input string"
+puts
+
+# 3.6
+arr.push('g')
+puts arr
+puts
+
+
